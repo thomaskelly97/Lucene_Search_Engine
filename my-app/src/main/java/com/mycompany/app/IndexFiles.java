5c70a719015b4f23f6fb05914d60eebca8c5b200
thomaskelly97 <tomskelly97@gmail.com>
2020-10-18 18:35:47 +0100

    SearchTest improvements & use cranfield

--- my-app/src/main/java/com/mycompany/app/IndexFiles.java
+++ my-app/src/main/java/com/mycompany/app/IndexFiles.java
@@ -13,6 +13,8 @@ import org.apache.lucene.document.TextField;
 import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.document.StringField;
+
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.index.IndexWriterConfig.OpenMode;
 import org.apache.lucene.queryparser.classic.ParseException;
@@ -50,7 +52,44 @@ public class IndexFiles {
         config.setOpenMode(IndexWriterConfig.OpenMode.CREATE_OR_APPEND);
         IndexWriter iwriter = new IndexWriter(directory, config);  
         Document document = new Document();
+        String currentLine = "";
+        String fieldName = "";
+
         try (BufferedReader br = new BufferedReader(new FileReader(inputFilePath))) {
+            currentLine = br.readLine();
+            while(currentLine != null){
+                System.out.println("ABSTRACT NUMBER: " + currentLine);
+                if (currentLine.matches("(.*)(.I)(.*)")) {
+                    document = new Document();
+                    Field pathField = new StringField("path", currentLine, Field.Store.YES);
+                    document.add(pathField);
+                    currentLine = br.readLine();
+                    System.out.println("Next line: " + currentLine);
+                    while (!currentLine.matches("(.*)(.I)(.*)")) {
+                        if (currentLine.matches("(.*)(.T)(.*)")) {
+                            fieldName = "Title";
+                            currentLine = br.readLine();
+                            System.out.println("Next line: " + currentLine);
+                        }
+                        else if (currentLine.matches("(.*)(.A)(.*)")) {
+                            fieldName = "Author";
+                            currentLine = br.readLine();
+                            System.out.println("Next line: " + currentLine);
+                        }
+                        else if (currentLine.matches("(.*)(.B)(.*)")) {
+                            fieldName = "Bibliography";
+                            currentLine = br.readLine();
+                        }
+                        else if (currentLine.matches("(.*)(.W)(.*)")) {
+                            fieldName = "Content";
+                            currentLine = br.readLine();
+                        }
+                        document.add(new TextField(fieldName, currentLine, Field.Store.YES));
+                        currentLine = br.readLine();
+                        if (currentLine == null) break;
+                    }
+                }
+            }
             document.add(new TextField("content", br));
             iwriter.addDocument(document);
             iwriter.close();
--- my-app/src/main/java/com/mycompany/app/SearchTest.java
+++ my-app/src/main/java/com/mycompany/app/SearchTest.java
@@ -22,6 +22,10 @@ import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.store.RAMDirectory;
+import org.apache.lucene.search.similarities.BM25Similarity;
+import org.apache.lucene.search.similarities.ClassicSimilarity;
+import java.util.Scanner;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -30,30 +34,61 @@ import java.nio.file.Paths;
 
 public class SearchTest {
   public static void main(String[] args) throws IOException, ParseException {
-      System.out.println("-> Start");
-      String outputDir = "/Users/user/Documents/College/5th_Year/InfoRet/Lucene_Search_Engine/index";
-      StandardAnalyzer analyzer = new StandardAnalyzer();
-      Directory directory = FSDirectory.open(Paths.get(outputDir));
-      
-      // Index reader reads from the index at 'directory'
-      IndexReader reader = DirectoryReader.open(directory);
-      // Searcher takes the index reader 
-      IndexSearcher searcher = new IndexSearcher(reader);
-      // Query parser parses the query 
-      QueryParser parser = new QueryParser("content", analyzer);
+    int scoring = 0; // Vector Space Model = 0; BM25 = 1
+    String outputDir = "/Users/user/Documents/College/5th_Year/InfoRet/Lucene_Search_Engine/index";
+    StandardAnalyzer analyzer = new StandardAnalyzer();
+    for (int i =0; i < args.length;i++) {
+      System.out.print(args[i]);
+      if ("-scoring".equals(args[i])) {
+        System.out.print("setting scoring to" + args[i+1]);
+        scoring = Integer.parseInt(args[i+1]);
+        i++;
+      }
+    }
+    
+  
+    System.out.println("SCORING: " + scoring);
+    Directory directory = FSDirectory.open(Paths.get(outputDir));
+    
+    // Index reader reads from the index at 'directory'
+    IndexReader reader = DirectoryReader.open(directory);
+    // Searcher takes the index reader 
+    IndexSearcher searcher = new IndexSearcher(reader);
+    if ((scoring) == 0) {
+      searcher.setSimilarity(new ClassicSimilarity());
+      System.out.println("Using VSM scoring.");
+    }
+    if ((scoring) == 1) {
+      searcher.setSimilarity(new BM25Similarity());
+      System.out.println("Using BM25 scoring.");
+    }
+    
+    Scanner scan = new Scanner(System.in);
+    while(true) {
+      System.out.print("> Please enter a query term:");
 
-      // Use the parser to parse a query 
-      Query query = parser.parse("Hello");
-      // Store result of using the query to search 
-      TopDocs results = searcher.search(query, 5);
-      System.out.println("Hits for hello: "+ results.totalHits);
+        // This method reads the number provided using keyboard
+      String term = scan.nextLine();
+      System.out.println("...searching for term: " + term);
+      QueryParser parser = new QueryParser("Title", analyzer);
+      Query query = parser.parse(term);
+      TopDocs results = searcher.search(query, 1400);
+      System.out.println("Hits for " + term + ": "+ results.totalHits);
+      System.out.println("RESULTS: " + results.scoreDocs);
+        // Closing Scanner after the use
+      }
+      // scan.close();
+    // Query parser parses the query 
 
-      query = parser.parse("superhero");
-      results = searcher.search(query, 5);
-      System.out.println("Hits for superhero: " + results.totalHits);
+    // Use the parser to parse a query 
+    // Store result of using the query to search 
 
-      query = parser.parse("buckling of transverse stiffened plates under shear");
-      results = searcher.search(query, 5);
-      System.out.println("Hits for buckling of transverse stiffened plates under shear: " + results.totalHits);
+    // query = parser.parse("critical shear");
+    // results = searcher.search(query, 5);
+    // System.out.println("Hits for superhero: " + results.totalHits);
+
+    // query = parser.parse("air flow in a separating laminar boundary layer");
+    // results = searcher.search(query, 5);
+    // System.out.println("Hits for buckling of transverse stiffened plates under shear: " + results.totalHits);
   }
 }
